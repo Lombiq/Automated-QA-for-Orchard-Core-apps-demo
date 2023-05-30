88f0474dfefc3cc120c165e416e98313c686e2c8
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-05-30 02:18:28 +0200

    Adding simple login UI test

---
+++ test/OrchardCoreQA.Demo.Web.Tests.UI/Tests/LoginTests.cs
@@ -0,0 +1,35 @@
+using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Constants;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OrchardCoreQA.Demo.Web.Tests.UI.Tests;
+
+public class LoginTests : UITestBase
+{
+    public LoginTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
+    {
+    }
+
+    [Theory, Chrome]
+    public Task LoginShouldWorkWithCorrectCredentials(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.GoToRelativeUrlAsync("/Login");
+
+                await context.FillInWithRetriesAsync(By.Id("UserName"), DefaultUser.UserName);
+                await context.FillInWithRetriesAsync(By.Id("Password"), DefaultUser.Password);
+
+                await context.ClickReliablyOnSubmitAsync();
+
+                (await context.GetCurrentUserNameAsync()).ShouldBe(DefaultUser.UserName);
+            },
+            browser);
+}
